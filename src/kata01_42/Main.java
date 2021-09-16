8a9e38b3b68f5ce74c8710900481fa5b418f476c
Entrar <Entrar@PC-1122>
2021-09-16 10:35:40 +0100

    Se usa LocalDate en lugar de Calendar.

--- src/kata01_42/Main.java
+++ src/kata01_42/Main.java
@@ -1,13 +1,14 @@
 package kata01_42;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Main {
 
     public static void main(String[] args) {
-        Calendar birthdate = GregorianCalendar.getInstance();
-        birthdate.set(1980, 1, 1);
+        LocalDate birthdate = LocalDate.of(1980, Month.JANUARY, 1);
         Person person = new Person("Antonio", birthdate);
         
         System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
--- src/kata01_42/Person.java
+++ src/kata01_42/Person.java
@@ -1,14 +1,15 @@
 package kata01_42;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Calendar birthdate;
+    private final LocalDate birthdate;
 
-    public Person(String name, Calendar birthdate) {
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -18,9 +19,7 @@ public class Person {
     }
 
     long getAge() {
-        long Milliseconds_per_Year = (long) (365.25*24*60*60*1000);
-        Calendar now = GregorianCalendar.getInstance();
-        return (now.getTimeInMillis()-birthdate.getTimeInMillis())/Milliseconds_per_Year;
+        return Period.between(birthdate, LocalDate.now()).getYears();
     }
     
     
